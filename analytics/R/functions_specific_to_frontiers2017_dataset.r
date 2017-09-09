46e3f06560907f3c60e1b3a20a87129ddd5369c8
Brian Cohn <brian.cohn@usc.edu>
2017-09-08 17:38:28 -0700

    #7 Moved all functions into official R package.

---
+++ analytics/R/functions_specific_to_frontiers2017_dataset.r
@@ -0,0 +1,12 @@
+# hardcoded specifically for
+# realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv
+save_snapshot_for_first_posture <- function(raw_data_timeseries_df, output_filepath = "../output/realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv") {
+  first_posture_data <- split_by_position(raw_data_timeseries_df$adept_x, first_data_chunk)[[1]]  #only get first posture
+  forces <- split_by_reference_force(first_posture_data)[2:101]
+  write.csv(first_posture_data, output_filepath, row.names = FALSE)
+}
+
+muscle_names <- c("M0", "M1", "M2", "M3", "M4", "M5", "M6")
+maximum_tendon_force <- 20
+minimum_tendon_force <- 3
+data_location <- "~/Resilio Sync/data/realTimeData2017_08_16_13_23_42.txt"
--- figures/functions_specific_to_frontiers2017_dataset.r
+++
@@ -1,7 +0,0 @@
-# hardcoded specifically for
-# realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv
-save_snapshot_for_first_posture <- function(raw_data_timeseries_df, output_filepath = "output/realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv") {
-  first_posture_data <- split_by_position(first_data_chunk$adept_x, first_data_chunk)[[1]]  #only get first posture
-  forces <- split_by_reference_force(first_posture_data)[2:101]
-  write.csv(first_posture_data, output_filepath, row.names = FALSE)
-}
