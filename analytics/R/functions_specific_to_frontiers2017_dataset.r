de6593a931781de0e311c7143c57bd60c56ae40c
Brian Cohn <brian.cohn@usc.edu>
2017-09-12 10:47:13 -0700

    #7 Completed documented function for acquiring dataset posture indices

--- analytics/R/functions_specific_to_frontiers2017_dataset.r
+++ analytics/R/functions_specific_to_frontiers2017_dataset.r
@@ -10,3 +10,17 @@ muscle_names <- c("M0", "M1", "M2", "M3", "M4", "M5", "M6")
 maximum_tendon_force <- 20
 minimum_tendon_force <- 3
 data_location <- "~/Resilio Sync/data/realTimeData2017_08_16_13_23_42.txt"
+
+##' @description 
+##' this function takes in the indices. the last final index is a 0, because it hasn't been included yet.
+##' This function fills that index with the correct value for each of the lines sampled.
+##' @param idxs list of two dataframes, that contain the coordinates of the points
+##' @return idxs_with_adept added adept_x and adept_y columns
+add_adept_xy_to_indices <- function(idxs) {
+  new_idxs <- idxs
+  new_idxs[[1]]$adept_x <- unique_postures$adept_x[1002:1208]
+  new_idxs[[1]]$adept_y <- unique_postures$adept_y[1002:1208]
+  new_idxs[[2]]$adept_x <- unique_postures$adept_x[2:1001]
+  new_idxs[[2]]$adept_y <- unique_postures$adept_y[2:1001]
+  return(new_idxs)
+}
