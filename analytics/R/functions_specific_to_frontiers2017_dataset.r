43b8a73405abf79302d6866c7694ef91f753ff45
bc <brian.cohn@usc.edu>
2017-10-07 10:41:26 -0700

    #14 Documented adept_coordinates and posture_indices_df

--- analytics/R/functions_specific_to_frontiers2017_dataset.r
+++ analytics/R/functions_specific_to_frontiers2017_dataset.r
@@ -26,10 +26,23 @@ add_adept_xy_to_indices <- function(idxs, unique_postures) {
   return(new_idxs)
 }
 
-posture_indices_df <- function(line_posture_start_indices){
-  final <- c(line_posture_start_indices[-1]-1, 0)
+##' Get AdeptX and AdeptY numeric tuple from first row of dataframe
+##' @param timeseries_df time series with the adept_x and adept_y columns
+##' @reutrn vector of adept_x,adept_y numeric values.
+adept_coordinates <- function(timeseries_df) {
+  df_head <- head(timeseries_df, 1)
+  adept_x <- df_head$adept_x
+  adept_y <- df_head$adept_y
+  return(c(adept_x, adept_y))
+}
+
+##' Compose start and final indices into a clean little dataframe
+##' @param line_postures_start_indices
+##' @return indices_df dataframe with the initial and final indices as the only elements
+posture_indices_df <- function(line_posture_start_indices) {
+  final <- c(line_posture_start_indices[-1] - 1, 0)
   initial <- c(line_posture_start_indices)
-  return(data.frame(initial = initial,final = final))
+  return(data.frame(initial = initial, final = final))
 }
 
 ##'Clean up the end of the postures
@@ -38,24 +51,19 @@ posture_indices_df <- function(line_posture_start_indices){
 ##' I did a manual bisection of the indices to find the end of the 1000 posture line (element 1 of the input)
 ##' @param idxs_of_lines_1_and_2 list of two elements, each with the initial, final idx from full_df.
 ##' @return idxs_of_lines_1_and_2_fixed last elements are hard-coded based on the initial data.
-  clean_up_posture_indices <- function(idxs_of_lines_1_and_2){
-    idxs <- idxs_of_lines_1_and_2 #make copy
-    idxs[[1]] <- idxs[[1]][-nrow(idxs[[1]]),]
-    idxs[[2]][1000,2] <- 81369527
+clean_up_posture_indices <- function(idxs_of_lines_1_and_2) {
+  idxs <- idxs_of_lines_1_and_2  #make copy
+  idxs[[1]] <- idxs[[1]][-nrow(idxs[[1]]), ]
+  idxs[[2]][1000, 2] <- 81369527
   return(idxs)
 }
 
 ##' These are the encoder values, which aren't used for Frontiers2017
-frontiers2017_columns_to_ignore <- c(
-  "angle_0",
-  "angle_1",
-  "angle_2",
-  "angle_3",
-  "angle_4",
-  "angle_5",
-  "angle_6"
-)
+frontiers2017_columns_to_ignore <- c("angle_0", "angle_1", "angle_2", "angle_3",
+  "angle_4", "angle_5", "angle_6")
 
 
 ##' These are the posture values, which are repeated across every data observation.
-columns_to_extract_into_attributes <- function() {c(unlist(lapply(muscle_names, reference)), "adept_x", "adept_y")}
+columns_to_extract_into_attributes <- function() {
+  c(unlist(lapply(muscle_names, reference)), "adept_x", "adept_y")
+}
