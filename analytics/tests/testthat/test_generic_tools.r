fb296ba0f9a85a83e3e7eab2d79dab5239f00898
Pavle Medvidovic <medvidov@usc.edu>
2017-10-23 18:54:49 -0700

    Added tests for shuffle_row_wise. Tests to make sure it works with: 1 row Mutliple rows Does not replace rows when shuffling

--- analytics/tests/testthat/test_generic_tools.r
+++ analytics/tests/testthat/test_generic_tools.r
@@ -4,6 +4,9 @@ context("Generic Tools")
 sample_df_numbers <- data.frame(first = c(1, 5, 4, 2, 5, 2), second = 1:6, row.names = c(5,
   2, 4, 3, 888, 827))
 sample_df_letters <- data.frame(first = c(1, 5, 4, 2, 5, 2), second = 1:6, row.names = LETTERS[1:6])
+sample_df_row <- data.frame(first = c(1), second = 1, row.names = LETTERS[1])
+sample_df_rows <- data.frame(first = c(1, 2, 3, 4, 5, 6), second = 1:6, row.names = LETTERS[1:6])
+shuffled_df_rows <- sample(nrow(sample_df_rows), 6, replace = FALSE, prob = NULL)
 
 context("first_rowname")
 test_that("first_rowname works with strings", {
@@ -19,4 +22,17 @@ test_that("last_rowname works with strings", {
 })
 test_that("last_rowname works with numbers", {
   expect_equal(last_rowname(sample_df_numbers), "827")
+})
+context("shuffle_row_wise")
+test_that("shuffle_row_wise works with one row", {
+  expect_equal(shuffle_row_wise(sample_df_row), sample_df_row)
+})
+test_that("shuffle_row_wise works with multiple rows", {
+  expect_equal(shuffle_row_wise(sample_df_rows), shuffled_df_rows)
+})
+test_that("shuffle_row_wise does not replace rows", {
+  expect_equal(shuffle_row_wise(sample_df_rows), shuffled_df_rows)
+})
+test_that("shuffle_row_wise works without replacement", {
+  expect_equal(shuffle_row_wise(sample_df_rows), shuffled_df_rows)
 })
