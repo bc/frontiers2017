efb3644c000158ce3513bf205dd89b86f9f91f9c
bc <brian.cohn@usc.edu>
2017-10-25 22:53:21 -0700

    #21 cleaned up linear fit tests

--- tests/testthat/test_linear_fit.r
+++ tests/testthat/test_linear_fit.r
@@ -1,4 +1,26 @@
 last_n_milliseconds = 100
+test_that("data for many postures can be used to create a list of A matrices",{
+  rds_postures <- all_file_paths("~/Resilio Sync/data/ForceTrials_at_each_posture/")
+  list_of_postures <- list_of_xy_to_df(pbmclapply(rds_postures, get_adept_coordinates_from_rds), c("adept_x","adept_y"))
+  list_of_A_matrices <- list_of_posture_rds_files_to_list_of_A_matrices(rds_postures, last_n_milliseconds)
+  vafs <- simplify2array(lapply(list_of_A_matrices, function(fit){
+    variance_accounted_for(fit[[2]],fit[[3]])
+  }))
+  cb <- data.frame(cbind(list_of_postures, vafs))
+  expect_equal(nrow(cb),1206)
+  fix_x_vaf <- cb[cb$adept_x == -525.000,]
+  fix_y_vaf <- cb[cb$adept_y == 68.000,]
+  expect_equal(nrow(fix_x_vaf),206)
+  expect_equal(nrow(fix_y_vaf),1000)
+  # Plot figure
+  fix_y <- posture_dependency_plot(fix_y_vaf, "adept_x", "vafs")
+  fix_x <- posture_dependency_plot(fix_x_vaf, "adept_y", "vafs")
+  require(gridExtra)
+  final <- gridExtra::grid.arrange(fix_y,fix_x, ncol = 2)
+  browser()
+  ggsave("../../output/posture_dependency_adept_xy.pdf", final, width = 14, height = 8, dpi=600)
+  ## end plot figure
+})
 
 test_that("a_matrix with fewer than all samples trained on <- forcetrial_list <- rds",{
   rds_folder_path <- "~/Resilio Sync/data/ForceTrials_at_each_posture/"
@@ -11,7 +33,6 @@ test_that("a_matrix with fewer than all samples trained on <- forcetrial_list <-
 })
 
 test_that("we can explore many different sample sizes for train/test splits", {
-
   sample_sizes <- 1:100
   model_performance <- 1:100 #TODO
   plot(sample_sizes, model_performance)
@@ -38,27 +59,6 @@ test_that("list of posture RDS paths to list of A matrices",{
   rds_postures <- simplify2array(lapply(dir(rds_folder_path), prepend_string,rds_folder_path))
 })
 
-test_that("data for many postures can be used to create a list of A matrices",{
-  rds_postures <- all_file_paths("~/Resilio Sync/data/ForceTrials_at_each_posture/")
-  list_of_postures <- list_of_xy_to_df(pblapply(rds_postures, get_adept_coordinates_from_rds), c("adept_x","adept_y"))
-  list_of_A_matrices <- list_of_posture_rds_files_to_list_of_A_matrices(rds_postures, last_n_milliseconds)
-  vafs <- simplify2array(lapply(list_of_A_matrices, function(fit){
-    variance_accounted_for(fit[[2]],fit[[3]])
-  }))
-  cb <- data.frame(cbind(list_of_postures, vafs))
-  expect_equal(nrow(cb),1206)
-  fix_x_vaf <- cb[cb$adept_x == -525.000,]
-  fix_y_vaf <- cb[cb$adept_y == 68.000,]
-  expect_equal(nrow(fix_x_vaf),206)
-  expect_equal(nrow(fix_y_vaf),1000)
-  ##Plot figure
-    fix_y <- posture_dependency_plot(fix_y_vaf, "adept_x", "vafs")
-    fix_x <- posture_dependency_plot(fix_x_vaf, "adept_y", "vafs")
-    require(gridExtra)
-    final <- gridExtra::grid.arrange(fix_y,fix_x, ncol = 2)
-    ggsave("../../../output/posture_dependency_adept_xy.pdf", final, width = 14, height = 8, dpi=600)
-    ## end plot figure
-})
 
 
 test_that("a_matrix <- forcetrial_list <- rds",{
