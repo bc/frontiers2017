c796f085019be6f1a73927900305241a67337beb
bc <brian.cohn@usc.edu>
2017-10-28 13:57:38 -0700

    #24 Data description no longer requires full_df directly

--- tests/testthat/test_data_description.r
+++ tests/testthat/test_data_description.r
@@ -6,9 +6,8 @@ source("../../R/functions_specific_to_frontiers2017_dataset.r")
 source("../../R/settling_time_analysis.r")
 
 test_that("Show a sample snapshot of the data collected", {
-  # Fig DataDescription Only take first part of data to create data description
-  # figure. (60e3 = first 60 seconds)
-  first_data_chunk <- read.csv(data_location, nrows = 85000, header = TRUE)
+  # first_data_chunk <- saveRDS(read.csv(data_location, nrows = 85000, header = TRUE), "first_data_chunk_85000_rows.rds")
+  first_data_chunk <- read_rds_to_package_extdata("first_data_chunk_85000_rows.rds")
   output_filepath <-  "../../output/realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv"
   # Save snapshot of just first posture
   save_snapshot_for_first_posture(first_data_chunk, output_filepath)
