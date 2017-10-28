911d6e8ad5b3f186ebc706ca2e0836a481f35677
Kian Jalaleddini <kian@mymotiv.com>
2017-10-28 12:25:29 -0700

    updating the frequency response functions

--- MATLAB stuff/analysisCoherence.m
+++ MATLAB stuff/analysisCoherence.m
@@ -22,30 +22,82 @@ forceOut = forceOut - mean(forceOut);
 inNum = size(forceRef,2);
 outNum = size(jr3Force,2);
 %%
+%calcualte bandwidth across muscles and see if there is a pattern as a
+%function of muscle
+%Confidence itnerval for a typical FRF
+%Activation time constant vs relaxation time constant
 linOrder = 2;
 data = iddata(jr3Force,forceRef,0.001);
-sysLinear = n4sid(data,linOrder,'Ts',0.001,'Focus','simulation','N4Weight','MOESP');
-jr3ForcePredict = dlsim(sys.A,sys.B,sys.C,sys.D,forceRef);
-axis = {'fX','fY','fZ'};
+sysIDLinear = n4sid(data,linOrder,'Ts',0.001,'Focus','simulation','N4Weight','MOESP');
+jr3ForcePredictLinear = compare(data,sysIDLinear,Inf);
+jr3ForcePredictLinear = jr3ForcePredictLinear.y;
+axisLabel = {'f_x','f_y','f_z'};
+%%
 disp('Linear system identification performance')
 for i = 1 : size(jr3Force,2)
-    disp(['VAF for ',axis{i},' was: ',num2str(vaf(jr3Force(:,i),jr3ForcePredict(:,i)))])
+    disp(['VAF for ',axisLabel{i},' was: ',num2str(vaf(jr3Force(:,i),jr3ForcePredictLinear(:,i)))])
 end
-bodePlot(sysLinear)
+sysID_SISO_Linear = cell(inNum,outNum);
+for i = 1 : outNum
+    subplot(3,1,i)
+    for j = 1 : inNum
+        thisSys = sysIDLinear(i,j);
+        thisSys.InputName = ['Muscle ',num2str(j)];
+        thisSys.OutputName = axisLabel(i);
+        if (j == 1)
+            h = bodeplot(thisSys);
+            setoptions(h,'FreqUnits','Hz','PhaseVisible','off');
+            hold on
+
+        else
+            bodeplot(thisSys);
+        end
+    end
+    xlim([0.1,500])
+end
+legend('M1','M2','M3','M4','M5','M6','M7')
 %%
-inputNL = poly1d('deg',10);
-linModel = idss(sys.A,sys.B,sys.C,sys.D,'Ts',0.001);
+A = abs(dcgain(sysIDLinear));
+figure
+hold on
+for i = 1 : inNum
+    quiver3(0,0,0,A((i - 1) * outNum + 1),A((i - 1) * outNum + 2),A((i - 1) * outNum + 3))
+end
+grid on
+xlabel('f_x')
+ylabel('f_y')
+zlabel('f_z')
+legend('M1','M2','M3','M4','M5','M6','M7')
+%%
+subplot(3,1,1)
+ylabel('Force x')
+box off
+subplot(3,1,2)
+ylabel('Force y')
+box off
+subplot(3,1,3)
+ylabel('Force z')
+xlabel('Frequency (Hz)')
+box off
+%%
+inputNL = poly1d('deg',4);
+linModel = idss(sysIDLinear.A,sysIDLinear.B,sysIDLinear.C,sysIDLinear.D,'Ts',0.001);
 outputNL = poly1d('deg',4);
-sys = nlhw(data,linModel,inputNL,[]);
-jr3ForcePredictBONL = compare(data,sys,Inf);
+opt = nlhwOptions('Display','on','SearchMethod','lm');
+opt.SearchOption.MaxIter = 50;
+
+%sysBONL = nlhw(data,linModel,inputNL,[],opt);
+sysBONL = nlhw(data,linModel,inputNL,outputNL);
+jr3ForcePredictBONL = compare(data,sysBONL,Inf);
 jr3ForcePredictBONL = jr3ForcePredictBONL.y;
+
 disp('Cascade system identification performance')
 for i = 1 : size(jr3Force,2)
-    disp(['VAF for ',axis{i},' was: ',num2str(vaf(jr3Force(:,i),jr3ForcePredictBONL(:,i)))])
+    disp(['VAF for ',axisLabel{i},' was: ',num2str(vaf(jr3Force(:,i),jr3ForcePredictBONL(:,i)))])
 end
-
+A = dcgain(sysIDLinear);
 %%
-plotJR3Prediction(jr3Force,jr3ForcePredict,fs)
+plotJR3Prediction(jr3Force,jr3ForcePredictLinear,fs)
 print('results/linearSysID.pdf','-dpdf')
 plotJR3Prediction(jr3Force,jr3ForcePredictBONL,fs)
 print('results/cascadeSysID.pdf','-dpdf')
