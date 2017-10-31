90f0c8c692bbeade2148af08385141b5ad0fb0d3
kian-ja <kian.ja@gmail.com>
2017-10-30 23:03:37 -0700

    not removing means from the data in identifying the model

--- R/a_matrix.r
+++ R/a_matrix.r
@@ -5,19 +5,20 @@ find_A_matrix <- function(data) {
   # The regressor matrix is concatenation of tendon forces
   time <- data[[1]]
   measured_muscle_col_names <- simplify2array(lapply(muscle_names(), measured))
-  raw_regressor <- as.matrix(data[measured_muscle_col_names])
-  regressor <- rm_mean_for_multiple_columns(raw_regressor)
-  regressor_means <- colMeans(raw_regressor)
-  raw_endpointForceObservation <- data[force_column_names]
-  endpointForceObservation <- rm_mean_for_multiple_columns(raw_endpointForceObservation)
-  response_means <- colMeans(raw_endpointForceObservation)
+  regressor <- as.matrix(data[measured_muscle_col_names])
+  num_observation <- nrow(regressor)
+  vector_one <- as.matrix(rep(1,num_observation),num_observation,1)
+  colnames(vector_one) <- 'offset'
+  regressor <- cbind(vector_one,regressor)
+  endpointForceObservation <- data[force_column_names]
   AMatrix <- matrix(solve(qr(regressor, LAPACK = TRUE), endpointForceObservation),
-  7, 6)
-  browser()
+  8, 6)
   endpointForcePrediction <- data.frame(regressor %*% AMatrix)
   colnames(endpointForcePrediction) <- force_column_names
   colnames(AMatrix) <- force_column_names
-  rownames(AMatrix) <- measured_muscle_col_names
+  rownames(AMatrix) <- colnames(regressor)
+  regressor_means <- colMeans(regressor)
+  response_means <- colMeans(endpointForceObservation)
   fit <- list(AMatrix = AMatrix, endpointForceObservation = endpointForceObservation,
     endpointForcePrediction = endpointForcePrediction, regressor_means = regressor_means,
   response_means = response_means)
