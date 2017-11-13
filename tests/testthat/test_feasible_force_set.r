209dac2fcd905b7294b22fb8a8cc087f3bb3c296
bc <brian.cohn@usc.edu>
2017-11-13 13:35:19 -0800

    #34 Fixed feasible input param

--- tests/testthat/test_feasible_force_set.r
+++ tests/testthat/test_feasible_force_set.r
@@ -7,13 +7,13 @@ test_that("experimentally collected output forces match the output of the H matr
   {
     sample <- input_output_per_posture[1:20]
     num_postures <- length(sample)
-    rgl_init(bg = "gray27")
+    rgl_init(bg = "white")
     list_of_mats <- add_gradient_to_attrs(lapply(sample, force3d_matrix, force_column_names[1:3]),
       gradient(num_postures))
 
     axes_for_multiple_sets(list_of_mats)
     # Add x, y, and z Axes
-    lapply(list_of_mats, function(mat) {
+    lapply(list_of_mats[c(1,5,7)], function(mat) {
       xyz_points_with_convhull(mat, col = attr(mat, "color"), points = FALSE)
     })
 
