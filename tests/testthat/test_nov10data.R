68822d8da48116eb9cabe3e4dead372f24e92ee2
bc <brian.cohn@usc.edu>
2017-11-17 21:04:26 -0800

    #39 Separated hit and run arguments

--- tests/testthat/test_nov10data.R
+++ tests/testthat/test_nov10data.R
@@ -32,9 +32,10 @@ data <- df_split_into_training_and_testing(input_output_data, fraction_training 
 training_data <- data$train
 test_data <- data$test
 
-force_names_to_predict <- c("JR3_FX","JR3_FY", "JR3_FZ")
+force_names_to_predict <- c("JR3_FX", "JR3_FY", "JR3_FZ")
 num_muscles <- 7
-A_fit <- find_A_matrix_without_offset(as.data.frame(training_data), reference(muscle_names()), force_names_to_predict)
+A_fit <- find_A_matrix_without_offset(as.data.frame(training_data), reference(muscle_names()),
+  force_names_to_predict)
 fit_evaluation_without_offset(A_fit, as.data.frame(test_data))
 
 range_tension <- c(3.1, 10.1)
@@ -44,97 +45,99 @@ generator_columns_A_matrix <- t(A_fit$AMatrix)
 dim(generator_columns_A_matrix)
 dim(muscle_constraints_matrix)
 
-task_multiplier <- c(0.9,2.8)
 task_force <- c(0.33152926, 0.07102741, 0.22046813)
-sset <- lapply(seq(0.9,2.8, length.out=10), function(i) {
-
-  one_observed_force <- task_force*i
-  big_A <- rbind(generator_columns_A_matrix,
-    -muscle_constraints_matrix, muscle_constraints_matrix)
-    big_b <- as.numeric(c(one_observed_force, rep(-range_tension[1],
-      num_muscles), rep(range_tension[2], num_muscles)))
-
-      dir_vector <- c(rep("=", length(one_observed_force)),
-      rep("<=", num_muscles),
-      rep("<=", num_muscles)
-    )
-    constr <- list(constr = big_A, dir = dir_vector, rhs = big_b)
-    constraints_are_feasible(constr)
-    state <- har.init(constr, thin = 100)
-    result <- har.run(state, n.samples = 100)
-    samples <- result$samples
-    #Try running those samples back through the A_matrix
-    t(A_fit$AMatrix) %*% t(samples)
-
-    #Show histograms of the FAS
-    par(mfrow=c(1,7))
-    lapply(1:7, function(muscle_num){
-      hist(samples[,muscle_num], breaks=10, main =paste("M",i, "at", i, collapse=""), xlab="Tendon force (N)", xlim=c(0,11))
-    })
-
-
-    lowest_l1_cost_soln <- samples[which.min(rowSums(samples)), ]
-    highest_l1_cost_soln <- samples[which.max(rowSums(samples)), ]
-
-    message('lowest l1 cost solution:')
-    message( format(lowest_l1_cost_soln,digits=2))
-    message('highest l1 cost solution:')
-    message( format(highest_l1_cost_soln,digits=2) )
-    test_predicted_response <- as.matrix(samples %*% A_fit$AMatrix)
-    boxplot(test_predicted_response, ylab = "Tension N for FX,FY,FZ, Torque Nm for MX,MY,MZ", main="what do most of the FAS-sampled forces product in output space? ")
-    plot3d(test_predicted_response)
-    #TODO get the test data from the actual data collected
-    # test_observed_response <- test_data[force_column_names]
-    # res_test <- test_observed_response - test_predicted_response
-    # summary(res_test)
-
-
-    # parcoord(samples)
-    rgl.clear()
-    plot3d(samples, xlim=c(0,11),ylim=c(0,11),zlim=c(0,11))  #show 3d plane
-    Sys.sleep(0.5)
-    return(samples)
-})
-    num_tasks <- length(sset)
-    rgl_init(bg = "white")
-    extract_3cols <- lapply(sset[c(1,2,5,8,10)], function(df) df[,5:7])
-    gradient <- colorRampPalette(c("#a6cee3", "#1f78b4", "#b2df8a", "#fc8d62", "#ffffb3",
-      "#bebada"))
-    list_of_mats <- add_gradient_to_attrs(extract_3cols,gradient(length(extract_3cols)))
-
-    axes_for_multiple_sets(list_of_mats)
-    # Add x, y, and z Axes
-    lapply(list_of_mats, function(mat) {
-      xyz_points_with_convhull(mat, col = attr(mat, "color"), points = FALSE)
-    })
+task_multiplier_bounds <- c(0.9, 2.8)
+task_multiplier_list <- seq(task_multiplier_bounds[1], task_multiplier_bounds[2],
+  length.out = 10)
+task_df <- t(task_force %*% t(task_multiplier_list))
+colnames(task_df) <- force_names_to_predict
 
 
 
+sset <- apply(task_df, 1, function(task_force) {
+  constr <- task_and_generators_to_constr(generator_columns_A_matrix, muscle_constraints_matrix, 
+    range_tension, task_force)
+  constraints_are_feasible(constr)
+  state <- har.init(constr, thin = 100)
+  result <- har.run(state, n.samples = 100)
+  samples <- result$samples
+  # Try running those samples back through the A_matrix
+  t(A_fit$AMatrix) %*% t(samples)
 
-
-  res <- lapply(sset, function(samples) {
-    cbound <- cbind(generate_map_creation_ids(nrow(samples)), as.data.frame(samples))
-    colnames(cbound) <- c("map_creation_id", muscle_names())
-    return(cbound)
+  # Show histograms of the FAS
+  par(mfrow = c(1, 7))
+  lapply(1:7, function(muscle_num) {
+    hist(samples[, muscle_num], breaks = 10, main = paste("M", muscle_num, "at",
+      format(task_force, digits = 3), collapse = ""), xlab = "Tendon force (N)",
+      xlim = c(0, 11))
   })
+
+
+  lowest_l1_cost_soln <- samples[which.min(rowSums(samples)), ]
+  highest_l1_cost_soln <- samples[which.max(rowSums(samples)), ]
+
+  message("lowest l1 cost solution:")
+  message(format(lowest_l1_cost_soln, digits = 2))
+  message("highest l1 cost solution:")
+  message(format(highest_l1_cost_soln, digits = 2))
+  test_predicted_response <- as.matrix(samples %*% A_fit$AMatrix)
+  boxplot(test_predicted_response, ylab = "Tension N for FX,FY,FZ, Torque Nm for MX,MY,MZ",
+    main = "what do most of the FAS-sampled forces product in output space? ")
+  plot3d(test_predicted_response)
+  # TODO get the test data from the actual data collected test_observed_response <-
+  # test_data[force_column_names] res_test <- test_observed_response -
+  # test_predicted_response summary(res_test)
+
+
+  # parcoord(samples)
+  rgl.clear()
+  plot3d(samples, xlim = c(0, 11), ylim = c(0, 11), zlim = c(0, 11))  #show 3d plane
+  Sys.sleep(0.5)
+  return(samples)
+})
+
+num_tasks <- length(sset)
+rgl_init(bg = "white")
+extract_3cols <- sset
+gradient <- colorRampPalette(c("#a6cee3", "#1f78b4", "#b2df8a", "#fc8d62", "#ffffb3",
+  "#bebada"))
+list_of_mats <- add_gradient_to_attrs(extract_3cols, gradient(length(extract_3cols)))
+
+browser()
+axes_for_multiple_sets(list_of_mats)
+# Add x, y, and z Axes
+lapply(list_of_mats, function(mat) {
+  xyz_points_with_convhull(mat, col = attr(mat, "color"), points = FALSE)
+})
+
+
+
+
+
+res <- lapply(sset, function(samples) {
+  cbound <- cbind(generate_map_creation_ids(nrow(samples)), as.data.frame(samples))
+  colnames(cbound) <- c("map_creation_id", muscle_names())
+  return(cbound)
+})
 
 big_har_set_to_test_on_finger <- dcrb(res)
 
-write.csv(big_har_set_to_test_on_finger, "scaling_task_n100_per_outputvec_of_interest_10_steps.csv", row.names=FALSE,quote=FALSE)
-#make a little db to remember which map was trying to achieve which task.
-tasklists <- lapply(seq(0.9,2.8, length.out=10), function(x) {
-  dcrb(rep(list(x*c(0.33152926, 0.07102741, 0.22046813)),100))
+write.csv(big_har_set_to_test_on_finger, "scaling_task_n100_per_outputvec_of_interest_10_steps.csv",
+  row.names = FALSE, quote = FALSE)
+# make a little db to remember which map was trying to achieve which task.
+tasklists <- lapply(seq(0.9, 2.8, length.out = 10), function(x) {
+  dcrb(rep(list(x * c(0.33152926, 0.07102741, 0.22046813)), 100))
 })
 task_list_df <- dcrb(tasklists)
 colnames(task_list_df) <- force_names_to_predict
-maps_with_target_tasks <- cbind(big_har_set_to_test_on_finger,task_list_df)
+maps_with_target_tasks <- cbind(big_har_set_to_test_on_finger, task_list_df)
 
-##TODO GET data from the cadaver finger from big_har_set_to_test_on_finger
+## TODO GET data from the cadaver finger from big_har_set_to_test_on_finger
 scaling <- as.data.frame(fread(get_Resilio_filepath("noPostureNeutralForceTrials2017_11_14_12_46_57.txt")))
 map_id_table_scaling <- as.data.frame(fread(get_Resilio_filepath("scaling_task_n100_per_outputvec_of_interest_10_steps_nov14.csv")))
 
 JR3_sensor_null <- colMeans(head(scaling, 30))
-#TODO test zero out sensors
+# TODO test zero out sensors
 sample_maps_data_scaling <- zero_out_JR3_sensors(scaling, JR3_sensor_null)
 p <- ggplot(data = head(sample_maps_data_scaling, 30))
 p <- p + geom_line(aes(time, JR3_FX))
@@ -151,7 +154,7 @@ are_correct_length <- dcc(lapply(dts, function(dt) {
 maps_scaling <- dts[are_correct_length]
 scaling_input_output_data <- as.data.frame(dcrb(lapply(lapply(maps, tail, 100), colMeans)))
 maps_with_target_tasks$map_creation_id <- as.numeric(as.character(maps_with_target_tasks$map_creation_id))
-a <- merge(scaling_input_output_data,maps_with_target_tasks,by="map_creation_id")
+a <- merge(scaling_input_output_data, maps_with_target_tasks, by = "map_creation_id")
 
 
 
