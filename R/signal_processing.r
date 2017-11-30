78f5e6ed15506c34cf6613f85763e88e3a5e6ccd
bc <brian.cohn@usc.edu>
2017-11-29 21:14:27 -0800

    #47 Instantiated lpf functionset

--- R/signal_processing.r
+++ R/signal_processing.r
@@ -4,3 +4,20 @@
 center_scale <- function(x) {
     scale(x, scale = FALSE)
 }
+
+
+##' simple low pass filter RC filter
+##' First order low pass filter. Sourced from http://biostatmatt.com/R/ecg.R
+##' @param y - vector to filter
+##' @param t - time interval between measurements (s)
+##' @param f - low pass frequency (Hz)
+lpf <- function( y, t, f ) {
+  rc <- 1 / ( 2 * pi * f )
+  a  <- t / ( t + rc )
+  n  <- length( y )
+  yf <- y
+  for( i in 2:length(y) ) {
+    yf[i] <- a * y[i] + (1-a) * yf[i-1]
+  }
+  return( yf )
+}
