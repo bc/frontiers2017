93daa5c7313d6b3f0574010244cd06c4f560ea9a
bc <brian.cohn@usc.edu>
2018-02-07 17:24:57 -0800

    #66 hand replicates produces plots successfully when testing the file.

--- tests/fulldata/pd_hand3_replicates_figure.r
+++ tests/fulldata/pd_hand3_replicates_figure.r
@@ -1,6 +1,4 @@
 context("evaluating_replicates")
-
-
 sections_of_interest <- dec20_PD_EXTMECH_maps_of_interest_by_section()
 signals_prefix <- "get_null_indices_via_this_plot_of_untransformed_xray_for_"
 
@@ -16,7 +14,6 @@ maps_of_interest <- sections_of_interest$replicates[, muscle_names()]
 colnames(maps_of_interest) <- reference(muscle_names())
 
 test_that("hand 3 ultraflex REPLICATES", {
-
   filename_3A <- "noiseResponse_ST1BC_2017_12_20_19_50_38_PD_Extmech_good_ultraflex_NOTAP.txt"
   hand3_dec20_ultraflex <- fread_df_from_Resilio(filename_3A)
   manual_3tap_for_hand3_ultraflex <- fread_df_from_Resilio("noiseResponse_ST1BC_2017_12_20_20_05_37_manual_3tap_extmech_for_ultraflex.txt")
@@ -25,14 +22,9 @@ test_that("hand 3 ultraflex REPLICATES", {
     input_are_voltages = TRUE, JR3_to_fingertip_distance = JR3_to_fingertip_distance,
     JR3_sensor_null = JR3_sensor_null)
       list_of_replicate_results <- replicate_df_list_from_noise_response(noise_response_wo_null, last_n_milliseconds)
-
-
   residual_sets_dt <- lapply(list_of_replicate_results, replicate_results_df_to_mean_6dim_magnitude_scatter, force_names_to_predict)
-p1 <- residual_from_mean_force_dimension_6dim(residual_sets_dt,force_names_to_predict=force_names_to_predict)
-ggsave(to_output_folder("replicate_meanwrench_residual_distributions.pdf"), p1, width=10, height=5, limitsize=FALSE)
-
+  p1 <- residual_from_mean_force_dimension_6dim(residual_sets_dt)
+  ggsave(to_output_folder("replicate_meanwrench_residual_distributions.pdf"), p1, width=10, height=5, limitsize=FALSE)
   p_magnitudes <- plot_histogram_of_magnitude_residuals(list_of_replicate_results)
   ggsave(to_output_folder("replicate_meanFxyz_magnitude_residuals.pdf"), p_magnitudes, width=3, height=3, limitsize=FALSE)
-
-
 })
